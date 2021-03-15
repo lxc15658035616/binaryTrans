fb891f0a2e5006484b2e74eca457c131111e6aff
li <1119896922@qq.com>
2021-03-15 21:06:56 +0800

    simple

--- index.js
+++ index.js
@@ -1,57 +1,65 @@
 #!/usr/bin/env node
-var http=require('http');
-var fs=require('fs/promises');
-var events=require('events');
-// http.createServer(function(request,response){
-//   response.writeHead(200,{"content-type":"text/html"});
-//   response.end('helloworld!');
-// }).listen(8888);
-var data=fs.readFile('./package.json');
-data.then(function(data){
-  var int16=new Int16Array(data);
-  //console.log(int16);
-  //eventEmitter.emit("sayhello");
-})
-var eventEmitter=new events.EventEmitter();
-eventEmitter.on("sayhello",function(){
-  console.log('helloworld');
-})
-// console.log(Buffer.alloc(10,1));
-// console.log(Buffer.from("人生自古誰無死","utf-8"));
-const arr = new Uint16Array(2);
+var format=[
+  {
+    key:"name",
+    terminal:",",
+    type:"str"
+  },
+  {
+    key:'age',
+    size:4,
+    type:'int',
+    end:'big'
+  },
+  {
+    key:'city',
+    type:'array',
+    sperator:',',
+    terminal:'.'
+  }
+]
+var buf=Buffer.allocUnsafe(100);
+var name='人生自古谁无死';
+var name_l=Buffer.byteLength(name);
+var yanji='延吉';
+var beijing='北京';
+var city_l=Buffer.byteLength(yanji)+Buffer.byteLength(beijing);
+buf.write(name,0);
+buf.write(',',name_l);
+buf.writeInt32BE(25,name_l+1);
+buf.write(yanji,name_l+5);
+buf.write(',',name_l+5+Buffer.byteLength(yanji));
+buf.write(beijing,name_l+5+Buffer.byteLength(yanji)+1);
+buf.write('.',name_l+5+Buffer.byteLength(yanji)+1+Buffer.byteLength(beijing));
+function trans(){
+  var res={};
+  let index_name=buf.indexOf(',');
+  res.name=buf.slice(0,index_name).toString();
+  res.age=buf.readInt32BE(index_name+1);
+  let city=[];
+  let start=index_name+5;
+  while(true){
+    let index1=buf.indexOf(',',start);
+    let index2=buf.indexOf('.',start);
+    console.log(index1,index2);
+    if(index1==-1){
+      if(index2!=-1){
+        city.push(buf.slice(start,index2).toString());
+        break;
+      }
+      break;
+    }
+    if(index1!=-1&&index2!=-1){
+      if(index1>index2){
+        break;
+      }
+    }
 
-arr[0] = 5000;
-arr[1] = 4000;
-
-// 拷贝 `arr` 的内容。
-const buf1 = Buffer.from(arr);
-
-// 与 `arr` 共享内存。
-const buf2 = Buffer.from(arr.buffer);
-//
-// console.log(buf1.length);
-// // 打印: <Buffer 88 a0>
-// console.log(buf2.length);
-// 打印: <Buffer 88 13 a0 0f>
-const buf = Buffer.from([1, 2, 3, 4]);
-const uint32array = new Uint32Array(
-    buf.buffer,
-    buf.byteOffset,
-    buf.length / Uint32Array.BYTES_PER_ELEMENT
-);
-// for(var v of buf.entries()){
-//   console.log(v);
-// }
-console.log(buf);
-
-buf.slice(0,1)[0]=2;
-console.log(buf);
-
-
-
-
-
-
-
-
+    city.push(buf.slice(start,index1).toString());
+    start=index1+1;
+  }
+  res.city=city;
+  console.log(res);
+}
+trans();
 //
